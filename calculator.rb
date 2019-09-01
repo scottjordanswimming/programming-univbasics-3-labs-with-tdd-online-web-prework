c70dbbc016f8665fd48b62af60f00d99186424f6
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-01 16:17:12 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = 2
+second_number = 1
+sum = second_number + first_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
